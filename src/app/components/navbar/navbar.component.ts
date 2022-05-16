a7eff3fdd2693fe8d0df16014bcfc34054a40e07
just-imah <ima.ntuen47@gmail.com>
2022-05-16 16:07:08 +0100

    update

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -11,17 +11,27 @@ export class NavbarComponent implements OnInit {
     // isMenuOpened: boolean = false;
    }
 
-  ngOnInit(): void {
-    // function sideBar()
-
-
-    
-  } 
+  ngOnInit(): void {} 
   
+  sideBar(data: any) {
+    console.log(data);
+    console.log(data.classList);
+      if (data.classList.contains('hidden')) {
+        data.classList.add('lg:inline-flex');
+      data.classList.add('flex');
+      data.classList.remove('hidden');
+      }
+    else {
+      data.classList.remove('lg:inline-flex');
+      data.classList.remove('flex');
+      data.classList.add('hidden');
+      }
+      console.log(data.classList);
+    }
     // toggleMenu(): void {
     //   this.isMenuOpened = !this.isMenuOpened;
     // }
-}
+  }
 
 // function Menu(e) {
 //       let list = document.querySelector('ul');
