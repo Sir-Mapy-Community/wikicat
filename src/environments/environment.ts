74d0eaf29636497a30d01080810c63595a9a9bb2
Samuel Ogu <hello@samuelogu.com>
2022-05-22 16:03:51 +0100

    content update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,15 @@
 export const environment = {
   production: false,
   social: {
-    twitter: '',
-    telegram: '',
-    instagram: '',
-    reddit: ''
+    twitter: 'https://twitter.com/wikicatcoin',
+    telegram: 'https://t.co/Yv8HV8rHn1',
+    instagram: 'https://instagram.com/wikicatcoin',
+    reddit: 'https://www.reddit.com/r/Wikicatcoin'
+  },
+  links: {
+    buy: 'https://pancakeswap.finance/swap?outputCurrency=0x6Ec90334d89dBdc89E08A133271be3d104128Edb',
+    contract: 'https://bscscan.com/token/0x6Ec90334d89dBdc89E08A133271be3d104128Edb',
+    chart: 'https://poocoin.app/tokens/0x6Ec90334d89dBdc89E08A133271be3d104128Edb'
   }
 };
 
