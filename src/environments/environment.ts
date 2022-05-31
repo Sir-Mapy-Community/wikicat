f4061209b621f70219ff09065d834114cc08db13
Samuel Ogu <hello@samuelogu.com>
2022-05-31 15:54:05 +0100

    update links

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,7 +14,7 @@ export const environment = {
     buy: 'https://pancakeswap.finance/swap?outputCurrency=0x6Ec90334d89dBdc89E08A133271be3d104128Edb',
     contract: 'https://bscscan.com/token/0x6Ec90334d89dBdc89E08A133271be3d104128Edb',
     lock: 'https://app.unicrypt.network/amm/pancake-v2/pair/0x933477eba23726cA95A957cB85dBB1957267EF85',
-    renounce: 'https://bscscan.com/token/0x6Ec90334d89dBdc89E08A133271be3d104128Edb',
+    renounce: 'https://www.bscheck.eu/bsc/0x6ec90334d89dbdc89e08a133271be3d104128edb',
     audit: 'https://solidity.finance/audits/WIKICAT/',
     chart: 'https://poocoin.app/tokens/0x6Ec90334d89dBdc89E08A133271be3d104128Edb'
   }
